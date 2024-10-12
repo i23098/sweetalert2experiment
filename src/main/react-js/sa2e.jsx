0c18ae69bf613fba43fc536d88a464dfc2394c78
Ricardo Almeida <ric.almeida@gmail.com>
2024-10-12 12:24:11 +0100

    Use sweetalert2 to show test error message

--- src/main/react-js/sa2e.jsx
+++ src/main/react-js/sa2e.jsx
@@ -1,6 +1,26 @@
 import React, { Component } from 'react';
+import Swal from 'sweetalert2';
 
 export default class SA2E extends Component {
+    componentDidMount() {
+        this.showError('Test error message');
+    }
+
+    async showError(message) {
+        try {
+            const rs = await Swal.fire({
+                icon: 'error',
+                title: 'Error!',
+                text: message,
+                backdrop: true
+            });
+
+            console.error('sweetalert2 finished', rs);
+        } catch (e) {
+            console.error('sweetalert2 error', e);
+        }
+    }
+
     render() {
         return <>Hello World!</>
     }
